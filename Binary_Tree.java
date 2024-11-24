48c61f53f887230265bf4164b98417150bfd79a7
Malay Maity <maitymalay27747@gmail.com>
2024-11-24 11:47:44 +0530

    Binary Tree using java

---
+++ Binary_Tree.java
@@ -0,0 +1,35 @@
+public class Binary_Tree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+
+        static int idx=-1;
+ 
+        public static Node buildTree(int Nodes[]){
+
+            idx++;
+            if(Nodes[idx]==-1){
+                return null;
+            }
+
+            Node newNode=new Node(Nodes[idx]);
+            newNode.left=buildTree(Nodes);
+            newNode.right=buildTree(Nodes);
+            return newNode;
+        }
+    }
+    public static void main(String args[]){
+        int Nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(Nodes);
+        System.out.println(root.data);
+    }
+}
